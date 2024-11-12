d29335bf2474e6b7599df81ceec60b84f18dea79
rubixsolver23 <tylerclaywatts@gmail.com>
2024-11-12 11:47:59 -0700

    finished the game!!!1!!!!1!

--- main.py
+++ main.py
@@ -11,10 +11,8 @@ screen = pygame.display.set_mode((400, 720))
 mainClock = pygame.time.Clock()
 
 SPAWNPIPE = pygame.USEREVENT
-#INCREASESPEED = pygame.USEREVENT
 
 pygame.time.set_timer(SPAWNPIPE, 2500)
-#pygame.time.set_timer(INCREASESPEED, 1000)
 
 game = Game("img/bird.png", "img/pipe.png", "img/background.png", "img/ground.png")
 game.resize_images()
@@ -26,18 +24,18 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and game.active:
-                   game.flap()
-        if event.type == pygame.MOUSEBUTTONDOWN:
+                game.flap()
+               
+            elif event.key == pygame.K_SPACE and not game.active:
+                game.restart()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
              if game.active:
                 game.flap()
-        if event.type == SPAWNPIPE:
+
+        elif event.type == SPAWNPIPE:
             game.spawn_pipe()
-
-        #if event.type == INCREASESPEED:
-         #   game.game_speed += 0.1
-
 
     
 
@@ -49,6 +47,10 @@ while True:
         game.move_pipes()
         game.show_pipes(screen)
         game.check_collision()
+        game.update_score()
+        game.show_score("playing", screen, (255,255,255))
+    else:
+        game.game_over(screen, (0,0,0))
 
 
 
